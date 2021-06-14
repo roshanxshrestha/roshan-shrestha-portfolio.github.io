b3dad562530a23a3543c331d45dc322118f2a755
Roshan Shrestha <85292792+roshanxshrestha@users.noreply.github.com>
2021-06-14 14:19:08 +0545

    code uploaded

---
+++ script.js
@@ -0,0 +1,70 @@
+window.addEventListener("load", function () {
+    /*--------------------- page loader-----------*/
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(function () {
+        document.querySelector(".page-loader").style.display = "none";
+    }, 600);
+    /*------------------animation on scroll------------------------*/
+    AOS.init();
+
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    // toggle menu/navbar
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i ').toggleClass("active");
+        menubar.classList.remove('active');
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Student", "Developer", "Blogger", "Video Editor", "Designer", "Sketch Artist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Student", "Developer", "Blogger", "Video Editor", "Designer", "Sketch Artist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+
+    });
+    // animation on scroll
+    AOS.init();
+});
+/*-----------------close nav when clicking on a  nav item------------*/
+document.addEventListener("click", function (e) {
+    if (e.target.closest(".menu")) {
+        toggleNav();
+    }
+});
